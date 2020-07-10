3e3b32f89860a432aad79be1166105742d776eab
effortsteven <effortsteven@gmail.com>
2020-07-10 19:39:40 +0300

    minor changes

--- core/settings.py
+++ core/settings.py
@@ -15,14 +15,14 @@ from django.conf import global_settings
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
-sentry_sdk.init(
-    dsn="https://543d5b10af6f4380915507238a10a3b8@o417908.ingest.sentry.io/5319701",
-    integrations=[DjangoIntegration()],
+# sentry_sdk.init(
+#     dsn="https://543d5b10af6f4380915507238a10a3b8@o417908.ingest.sentry.io/5319701",
+#     integrations=[DjangoIntegration()],
 
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
-)
+#     # If you wish to associate users to errors (assuming you are using
+#     # django.contrib.auth) you may enable sending PII data.
+#     send_default_pii=True
+# )
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
