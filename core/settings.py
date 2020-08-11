f4d49afef8224eb2d492b8d12225128911661cbb
Ismaili1995 <ismailimohamedi1995@gmail.com>
2020-08-11 12:42:02 +0300

    adding short url

--- core/settings.py
+++ core/settings.py
@@ -15,14 +15,14 @@ from django.conf import global_settings
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
-# sentry_sdk.init(
-#     dsn="https://543d5b10af6f4380915507238a10a3b8@o417908.ingest.sentry.io/5319701",
-#     integrations=[DjangoIntegration()],
+sentry_sdk.init(
+    dsn="https://543d5b10af6f4380915507238a10a3b8@o417908.ingest.sentry.io/5319701",
+    integrations=[DjangoIntegration()],
 
-#     # If you wish to associate users to errors (assuming you are using
-#     # django.contrib.auth) you may enable sending PII data.
-#     send_default_pii=True
-# )
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -120,33 +120,33 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 
 # Production Database
-# DATABASES = {
-#    'default': {
-
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-
-#         'NAME': 'core',
-
-#         'USER': 'postgres',
-
-#         'PASSWORD': 'b@11game',
-
-#         'HOST': '157.245.12.198',
-
-#     }
-# }
-
-# development Database 2
 DATABASES = {
    'default': {
+
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'cores',
+
+
+        'NAME': 'core',
+
         'USER': 'postgres',
-        'PASSWORD': 'Namwinyu1995',
-        'HOST': 'localhost',
+
+        'PASSWORD': 'b@11game',
+
+        'HOST': '157.245.12.198',
+
     }
 }
+
+# development Database 2
+# DATABASES = {
+#    'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'cores',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Namwinyu1995',
+#         'HOST': 'localhost',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
